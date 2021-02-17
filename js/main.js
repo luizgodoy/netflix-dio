f35b29221f25f05839a874e87883603a0c632b65
Luiz Godoy <garciadegodoy@msn.com>
2021-02-17 07:56:00 -0300

    Recriando a página principal do NetFlix, usando responsividade nas páginas. Páginas estáticas.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function abrir(){
+    window.open("https://www.youtube.com/watch?v=ff-XiZzJLxw");
+}
+
+function detalhes(){
+    alert("Na história de Elementar o detetive, um homem excêntrico que foi consultor da Scotland Yard, vai para Nova Iorque para passar um período em um centro de reabilitação. Quando deixa a clínica ele decide morar no Brooklyn na companhia de Joan, uma ex-cirurgiã que perdeu sua licença quando um paciente morreu. Na época, ela prestava auxílio para o Departamento de Polícia de Nova Iorque. Joan deverá representar para Sherlcok a pessoa que o manterá sóbrio.");
+}
